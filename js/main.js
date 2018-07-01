a7df17a786c867751b6e1c93a8f40d1b52f7b085
Wojtek Dudek <wdudek82@gmail.com>
2018-07-01 14:55:44 +0200

    Changed styles for hovered and dropped image

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ function dragStart() {
 }
 
 
-function dragEnter() {
+function dragEnter(e) {
+  e.preventDefault();
   this.classList.add('hovered');
 }
 
 function dragOver(e) {
   e.preventDefault();
-  // this.classList.add('hovered');
 }
 
 function dragLeave() {
@@ -22,6 +22,7 @@ function dragLeave() {
 }
 
 function dragDrop() {
+  this.classList.remove('hovered');
   this.appendChild(fill);
 }
 
