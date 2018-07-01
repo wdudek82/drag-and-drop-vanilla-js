19558b97b90600612d86162ad8d39bae84b10788
Wojtek Dudek <wdudek82@gmail.com>
2018-07-01 14:30:03 +0200

    Image is now draggable, appended to drop area, removed from the source

--- js/main.js
+++ js/main.js
@@ -3,38 +3,41 @@ const empties = document.querySelectorAll('.empty');
 
 // Drag Functions
 function dragStart() {
-  console.log('start');
   this.classList.add('hold');
-
   setTimeout(() => this.classList.add('invisible'), 0);
 }
 
-function dragEvent() {
-  console.log('dragged');
-}
-
-function dragEnd() {
-  console.log('end');
-  this.classList.remove('hold');
-  this.classList.remove('invisible');
-}
 
 function dragEnter() {
   this.classList.add('hovered');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  // this.classList.add('hovered');
 }
 
 function dragLeave() {
   this.classList.remove('hovered')
 }
 
+function dragDrop() {
+  this.appendChild(fill);
+}
+
+function dragEnd() {
+  this.classList.remove('hold');
+  this.classList.remove('invisible');
+}
+
 // Fill Listeners
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
-fill.addEventListener('dragged', dragEvent);
 
 // Empties Listeners
-console.log(empties);
 empties.forEach((empty) => {
   empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragover', dragOver);
   empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
 });
